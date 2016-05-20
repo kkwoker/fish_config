bc9db480d55b46fce0bb900b6b207f22892fd3fd
Kinnan Kwok <kkwoker@gmail.com>
2016-05-20 15:03:15 -0700

    Add chruby fish files

--- config.fish
+++ config.fish
@@ -1,3 +1,6 @@
 source ~/.config/fish/fish_prompt
 source ~/.config/fish/fish_colors
 source ~/.config/fish/fish_git_prompt
+
+source /usr/local/share/chruby/chruby.fish
+source /usr/local/share/chruby/auto.fish
