3b44c9ad1865c2c0f159e0e12d18a07302891b8e
Kinnan Kwok <kkwoker@gmail.com>
2016-05-20 14:39:14 -0700

    Move git prompts and colors into its own file

--- config.fish
+++ config.fish
@@ -1,4 +1,3 @@
 source ~/.config/fish/fish_prompt
-
-set __first_git_prompt_showcolorhints 'yes'
-set __fish_git_prompt_show_informative_status 'yes'
+source ~/.config/fish/fish_colors
+source ~/.config/fish/fish_git_prompt
