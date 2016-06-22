8eecb86a6ed0c9d8fcb4754a83387aedf1dfd80f
Kinnan Kwok <kkwoker@gmail.com>
2016-06-22 12:19:57 -0700

    Use chruby and use ll function

--- config.fish
+++ config.fish
@@ -2,5 +2,15 @@ source ~/.config/fish/fish_prompt
 source ~/.config/fish/fish_colors
 source ~/.config/fish/fish_git_prompt
 
+# chruby
 source /usr/local/share/chruby/chruby.fish
-source /usr/local/share/chruby/auto.fish
+# source /usr/local/share/chruby/auto.fish
+chruby 2.3.0
+
+# Custom
+source ~/.config/fish/credentials
+source ~/.config/fish/ll.fish
+
+fish_vi_mode
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
