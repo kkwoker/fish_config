b73b65535aee7568862ce2641e92b839b9097fb8
Kinnan Kwok <kkwoker@gmail.com>
2016-09-10 11:06:05 -0700

    Add golang, subl and update vi_key_bindings

--- config.fish
+++ config.fish
@@ -5,12 +5,22 @@ source ~/.config/fish/fish_git_prompt.fish
 # chruby
 source /usr/local/share/chruby/chruby.fish
 # source /usr/local/share/chruby/auto.fish
-chruby 2.3.0
+chruby 2.3.1
 
 # Custom
 source ~/.config/fish/credentials.fish
 source ~/.config/fish/ll.fish
 
-fish_vi_mode
+# Golang
+source ~/.config/fish/golang.fish
+
+# Workspaces
+source ~/.config/fish/workspaces.fish
+fish_vi_key_bindings
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
+# Sublime
+function subl
+  eval "/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl" $argv
+end
